b7c4250ddfa870dd684d2fec53c492a6e89c2c87
imacul77 <imacull77@gmail.com>
2024-02-23 00:09:44 -0800

    new update

--- middleware.ts
+++ middleware.ts
@@ -3,16 +3,8 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
     publicRoutes: [
         '/',
-        '/events/:id',
         '/api/webhooks/clerk',
-        '/api/webhooks/clerk',
-        '/api/webhooks/stripe',
     ],
-
-    ignoredRoutes: [
-    '/api/webhooks/clerk',
-    '/api/webhooks/stripe',
-    ]
 });
  
 export const config = {
